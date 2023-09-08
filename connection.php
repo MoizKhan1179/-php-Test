58aae5a4cb625a5c1b1e354eb4be9b0139dfd486
Abdul Moiz <120182920+MoizKhan1179@users.noreply.github.com>
2023-09-08 10:54:14 +0500

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,12 @@
+<?php
+// servername, username, password, databasename
+$connect = mysqli_connect("localhost", "root", "", "insertion");
+
+if(!$connect){
+    die("connection failed");
+}
+
+
+
+
+?>
